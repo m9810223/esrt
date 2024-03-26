a9cbd9f45807711461432fea7cd9fac760bd811a
Michael <m9810223@gmail.com>
2024-03-26 18:51:50 +0800

    feat: release

--- src/esrt/__main__.py
+++ src/esrt/__main__.py
@@ -20,9 +20,7 @@ from .utils import parse_params
 
 app = typer.Typer(
     no_args_is_help=True,
-    context_settings=dict(
-        help_option_names=['-h', '--help'],
-    ),
+    context_settings={'help_option_names': ['-h', '--help']},
     pretty_exceptions_enable=False,
 )
 
