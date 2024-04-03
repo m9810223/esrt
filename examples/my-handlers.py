8f7489053218a7e211f0399e05548a1ef5ab57cf
Michael <m9810223@gmail.com>
2024-04-03 14:00:05 +0800

    feat: release

--- examples/my-handlers.py
+++ examples/my-handlers.py
@@ -4,15 +4,6 @@ import typing as t
 from esrt import DocHandler
 
 
-class MyHandler(DocHandler):
-    def handle_one(self, action: str):
-        obj = json.loads(action)
-        prefix = 'new-'
-        if not t.cast(str, obj['_index']).startswith(prefix):
-            obj['_index'] = prefix + obj['_index']
-        return obj
-
-
 # function style
 def my_handler(actions: t.Iterable[str]):
     for action in actions:
@@ -21,3 +12,16 @@ def my_handler(actions: t.Iterable[str]):
         if not t.cast(str, obj['_index']).startswith(prefix):
             obj['_index'] = prefix + obj['_index']
         yield obj
+
+# class style
+class MyHandler(DocHandler):
+    def handle(self, actions: t.Iterable[str]):
+        for action in actions:
+            yield self.handle_one(action)
+
+    def handle_one(self, action: str):
+        obj = json.loads(action)
+        prefix = 'new-'
+        if not t.cast(str, obj['_index']).startswith(prefix):
+            obj['_index'] = prefix + obj['_index']
+        return obj
