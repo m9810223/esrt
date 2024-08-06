8641785d414040ea7345b8575272f7734b130bdf
Michael <m9810223@gmail.com>
2024-08-06 16:46:54 +0800

    feat: refine host

--- src/esrt/es.py
+++ src/esrt/es.py
@@ -3,8 +3,9 @@ from elasticsearch import Elasticsearch
 
 class Client(Elasticsearch):
     def __init__(self, *, host: str):
-        if not host.startswith('http'):
-            host = 'http://' + host
-        if ':' not in host.split('://')[-1]:
-            host += ':9200'
+        is_https = host.startswith('https://')
+        has_port = ':' in host.split('://')[-1]
+        if not is_https:
+            if not has_port:
+                host += ':9200'
         super().__init__(hosts=host)
