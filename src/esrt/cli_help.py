6a37f37eba02ba2b64aeee5447f1b7bcd6b77c35
Michael <m9810223@gmail.com>
2024-08-08 20:49:23 +0800

    feat: refactor
    BREAKING CHANGE: refactor

--- src/esrt/cli_help.py
+++ src/esrt/cli_help.py
@@ -23,24 +23,9 @@ class Help:
     _width = Command.width() + 1
     _fmt = f'{{:{_width}}}'
 
-    e_search = typer.style(
-        typer.style(_fmt.format(Command.search), fg=typer.colors.MAGENTA, bold=True)
-        + typer.style('Elasticsearch.search', bold=True, dim=True, italic=True),
-    )
-    s_scan = typer.style(
-        typer.style(_fmt.format(Command.scan), fg=typer.colors.MAGENTA, bold=True)
-        + typer.style('helpers.scan', bold=True, dim=True, italic=True)
-    )
-    r_request = typer.style(
-        typer.style(_fmt.format(Command.request), fg=typer.colors.MAGENTA, bold=True)
-        + typer.style('Transport.perform_request', bold=True, dim=True, italic=True),
-    )
-    t_transmit = typer.style(
-        typer.style(_fmt.format(Command.transmit), fg=typer.colors.MAGENTA, bold=True)
-        + typer.style('helpers.streaming_bulk', bold=True, dim=True, italic=True)
-    )
+    e_search = typer.style(typer.style(_fmt.format(Command.search), fg=typer.colors.MAGENTA, bold=True) + typer.style('Elasticsearch.search', bold=True, dim=True, italic=True))
+    s_scan = typer.style(typer.style(_fmt.format(Command.scan), fg=typer.colors.MAGENTA, bold=True) + typer.style('helpers.scan', bold=True, dim=True, italic=True))
+    r_request = typer.style(typer.style(_fmt.format(Command.request), fg=typer.colors.MAGENTA, bold=True) + typer.style('Transport.perform_request', bold=True, dim=True, italic=True))
+    t_transmit = typer.style(typer.style(_fmt.format(Command.transmit), fg=typer.colors.MAGENTA, bold=True) + typer.style('helpers.streaming_bulk', bold=True, dim=True, italic=True))
 
-    sql = typer.style(
-        typer.style(_fmt.format(Command.sql), fg=typer.colors.MAGENTA, bold=True)
-        + typer.style('request -X POST /_sql', bold=True, dim=True, italic=True),
-    )
+    sql = typer.style(typer.style(_fmt.format(Command.sql), fg=typer.colors.MAGENTA, bold=True) + typer.style('request -X POST /_sql', bold=True, dim=True, italic=True))
