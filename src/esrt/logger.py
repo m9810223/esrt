526c5456ad796cc751b9b5a81144aa693f433309
Michael <m9810223@gmail.com>
2024-08-08 23:22:11 +0800

    chore: logging color

--- src/esrt/logger.py
+++ src/esrt/logger.py
@@ -1,13 +1,15 @@
 import logging
 import sys
 
+import typer
+
 
 logger = logging.getLogger('esrt')
 
 
 def set_log_level(level: str):
     logging.basicConfig(
-        format='{levelname:<7} ' '[{asctime}] ' '{message} ',
+        format=typer.style('{levelname:<7} ' '[{asctime}] ' '{message} ', dim=True),
         datefmt='%H:%M:%S',
         style='{',
         level=level,
